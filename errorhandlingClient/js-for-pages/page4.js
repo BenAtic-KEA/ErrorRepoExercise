dfa9a90f3779e83ed46ac7e72bffecab0723b07d
BenAtic-KEA <mark024e@stud.kea.dk>
2022-03-30 11:45:24 +0200

    error handling

--- errorhandlingClient/js-for-pages/page4.js
+++ errorhandlingClient/js-for-pages/page4.js
@@ -1,5 +1,5 @@
 import { SERVER } from "../settings.js"
-import { handleHttpErrors } from "../fetchUtils.js"
+import { handleHttpErrors, makeOptions } from "../fetchUtils.js"
 const SERVER_URL = SERVER + "/api/quotes"
 
 export function page4Handlers() {
@@ -9,58 +9,52 @@ export function page4Handlers() {
 }
 
 
-function findQuote() {
+async function findQuote() {
+  document.getElementById("quote").value = ""
+  document.getElementById("author").value = ""
+
   const id = getIdFromInputField()
-  fetch(`${SERVER_URL}/${id}`)
-    .then(res => {
-      if (!res.ok) {
-        throw new Error("Could not find quote (")
-      }
-      return res.json()
-    })
-    .then(foundQuote => {
+  try{
+
+  const foundQuote = await fetch(`${SERVER_URL}/${id}`)
+    .then(res => handleHttpErrors(res))
+    
       document.getElementById("quote").value = foundQuote.quote
       document.getElementById("author").value = foundQuote.ref
-    })
-    .catch(e => alert(e.message + " (NEVER use alerts for real)"))
+  }catch (error){
+    document.getElementById("error").innerText = error.message
+  }    
+    
 }
 
-function editQuote() {
+async function editQuote() {
   const id = getIdFromInputField()
   const editedQuote = {
     id: id
   }
   editedQuote.quote = document.getElementById("quote").value
   editedQuote.ref = document.getElementById("author").value
-
-  fetch(SERVER_URL + "/" + id, {
-    method: "PUT",
-    headers: {
-      "Accept": "application/json",
-      "Content-type": "application/json"
-    },
-    body: JSON.stringify(editedQuote)
-  })
-    .then(res => {
-      if (!res.ok) {
-        throw new Error("Error while editing the quote")
-      }
-      return res.json()
-    })
+  
+  try{
+  const quote = await fetch(SERVER_URL + "/" + id, makeOptions("PUT",editedQuote))
+    .then(res => handleHttpErrors(res))
     .then(() => clearFields())
-    .catch(err => alert(err.message + " (NEVER USE ALERT FOR REAL)"))
-
+  }catch (error){
+    document.getElementById("error").innerHTML = error.message
+  }
 
 }
 async function deleteQuote() {
   const id = getIdFromInputField()
-  await fetch(SERVER_URL + "/" + id, {
-    method: "DELETE"
-  }).then(res => {
-    res.text()
-  })
-  clearFields()
-}
+  try{
+
+    const deleteQuote = await fetch(SERVER_URL + "/" + id, makeOptions("DELETE"))
+    .then(res => handleHttpErrors(res))
+    .then(() => clearFields())
+  }catch (error){
+    document.getElementById("error").innerText = error.message
+  }
+  }
 
 function clearFields() {
   document.getElementById("quote-id").value = ""
