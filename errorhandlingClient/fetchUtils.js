8ae9a9feda7823491480fa9e4048ac19570ec275
BenAtic-KEA <mark024e@stud.kea.dk>
2022-03-30 11:16:40 +0200

    utils

--- errorhandlingClient/fetchUtils.js
+++ errorhandlingClient/fetchUtils.js
@@ -7,3 +7,17 @@ export async function handleHttpErrors(res) {
     }
     return res.json()
   }
+
+  export function makeOptions(method, body) {
+    const opts = {
+      method: method,
+      headers: {
+        "Content-type": "application/json",
+        "Accept": "application/json"
+      }
+    }
+    if (body) { //Observe how we can add new fields to an object when needed
+      opts.body = JSON.stringify(body);
+    }
+    return opts;
+  }
--- errorhandlingClient/js-for-pages/page3.js
+++ errorhandlingClient/js-for-pages/page3.js
@@ -1,5 +1,5 @@
 import { SERVER } from "../settings.js"
-import { handleHttpErrors } from "../fetchUtils.js";
+import { handleHttpErrors, makeOptions } from "../fetchUtils.js";
 const API_URL = SERVER + "/api/quotes"
 
 export function setUpAddButtonHandler() {
@@ -7,21 +7,15 @@ export function setUpAddButtonHandler() {
 }
 
 async function addNewQuote() {
-  try{
+  document.getElementById("error").innerHTML = ""
   const newQuote = {};
   newQuote.quote = document.getElementById("quote").value
   newQuote.ref = document.getElementById("author").value
-
-  fetch(API_URL, {
-    method: "POST",
-    headers: {
-      "Accept": "application/json",
-      "Content-type": "application/json"
-    },
-    body: JSON.stringify(newQuote)
-  })
+  try{
+  const addedQuote = await fetch(API_URL, makeOptions("Post",newQuote))
     .then(res => handleHttpErrors(res))
-    .then(addedQuote => document.getElementById("addedQuote").innerText = JSON.stringify(addedQuote))
+
+   document.getElementById("addedQuote").innerText = JSON.stringify(addedQuote)
   
 }catch (error){
   document.getElementById("error").innerHTML = error.message
